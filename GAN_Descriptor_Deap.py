1800e87b912f74d416f2c242a0de3f04a5ad2d26
Unai Garciarena <unai.garciarena@ehu.es>
2019-11-27 12:12:24 +0100

    Update GAN_Descriptor_Deap.py

--- GAN_Descriptor_Deap.py
+++ GAN_Descriptor_Deap.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 import tensorflow as tf
 import numpy as np
@@ -11,7 +9,7 @@ from deap import base
 from deap import creator
 from deap import tools
 import copy
-from tunable_gan_class_optimization import xavier_init, GAN, GANDescriptor
+from tunable_gan_class_optimization import xavier_init, GAN, GANDescriptor, Divergence_Fs
 from Class_F_Functions import pareto_frontier, FFunctions, igd
 import os
 import matplotlib.pyplot as plt
@@ -22,7 +20,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # to make this notebook's output stable across runs
 
 
-def reset_graph(seed=42):
+def reset_graph():
     global All_Evals
     tf.reset_default_graph()
     tf.set_random_seed(All_Evals)
@@ -49,11 +47,11 @@ def eval_gan_approx_igd(individual):
     global test1
     global test2
 
-    reset_graph(myseed)
+    reset_graph()
 
     start_time = time.time()
-    my_gan_descriptor = individual.gan_descriptor
-    my_gan = GAN(my_gan_descriptor)
+    descriptor = individual.gan_descriptor
+    my_gan = GAN(descriptor)
     my_gan.training_definition()
     final_samples = my_gan.separated_running("fixed", ps_all_x, mb_size, number_epochs,  number_samples, print_cycle)
     elapsed_time = time.time() - start_time
@@ -65,9 +63,10 @@ def eval_gan_approx_igd(individual):
     tf1, tf2, _ = pareto_frontier(nf1, nf2)
 
     igd_val = igd((np.vstack((tf1, tf2)).transpose(), np.vstack((test1, test2)).transpose()))[0]
+
     All_Evals = All_Evals+1
 
-    gan_code = my_gan_descriptor.codify_components(nlayers, init_functions, act_functions, divergence_measures, lat_functions)
+    gan_code = descriptor.codify_components(n_layers, init_functions, act_functions, div_measures, lat_functions)
     print("Eval:",  All_Evals, " PS_Size:", len(tf1), " Fitness:",  igd_val, " Time:", elapsed_time)
     if All_Evals == 1:
         Eval_Records = np.array(gan_code+[All_Evals, len(tf1), igd_val, elapsed_time])
@@ -77,7 +76,7 @@ def eval_gan_approx_igd(individual):
     if igd_val < best_val:
         best_val = igd_val
 
-        fig = plt.figure(figsize=(5,5))
+        fig = plt.figure(figsize=(5, 5))
         ax = fig.add_subplot(111)
         ax.set_xlabel('$f(x_1)$', fontsize=25)
         ax.set_ylabel('$f(x_2)$', fontsize=25)
@@ -87,11 +86,8 @@ def eval_gan_approx_igd(individual):
         plt.yticks(np.arange(0, np.max(nf1), 0.5), fontsize=20)
         plt.plot(nf1, nf2, 'r.')
         plt.text(0, 0, str(igd_val)+' -- '+str(len(tf1)), size=15)
-        #plt.show()
-        fig.savefig("DeapGAN_"+str(my_gan_descriptor.fmeasure)+"_"+Function+'_eval_'+str(All_Evals)+'.pdf')
+        fig.savefig("DeapGAN_"+str(descriptor.f_measure)+"_"+Function+'_eval_'+str(All_Evals)+'.pdf')
         plt.close()
-
-        # my_gan_descriptor.print_components()
 
     return igd_val, elapsed_time
 
@@ -100,52 +96,33 @@ def eval_gan_approx_igd(individual):
 
 def init_individual(ind_class):
 
-    generator_n_hidden = np.random.randint(nlayers)+1                                 # Number of hidden layers
-    discriminator_n_hidden = np.random.randint(nlayers)+1                             # Number of hidden layers
+    gen_n_hidden = np.random.randint(1, n_layers+1)                                 # Number of hidden layers
+    disc_n_hidden = np.random.randint(1, n_layers+1)                             # Number of hidden layers
 
-    generator_dim_list = [np.random.randint(max_layer_size)+1 for _ in range(generator_n_hidden)]
-    discriminator_dim_list = [np.random.randint(max_layer_size)+1 for _ in range(discriminator_n_hidden)]
+    gen_dims = (np.random.randint(1, max_layer_size+1, gen_n_hidden)).tolist()
+    disc_dims = (np.random.randint(1, max_layer_size+1, disc_n_hidden)).tolist()
 
-    gen_number_loop_train = np.random.randint(nloops)+1                            # Number loops for training generator
-    disc_number_loop_train = np.random.randint(nloops)+1                        # Number loops for training discriminator
+    gen_loop = np.random.randint(n_loops)+1   # Number loops for training generator
+    disc_loop = np.random.randint(n_loops)+1  # Number loops for training discriminator
 
-    # divergence_measures = ["Standard_Divergence","Total_Variation","Forward_KL","Reverse_KL","Pearson_Chi_squared","Squared_Hellinger","Least_squared"]
+    f_measure = div_measures[np.random.randint(len(div_measures))]
 
-    fmeasure = divergence_measures[np.random.randint(len(divergence_measures))]
+    gen_init_fs = (np.random.choice(init_functions, gen_n_hidden+1)).tolist()
+    disc_init_fs = (np.random.choice(init_functions, disc_n_hidden+1)).tolist()
 
-    i_g_function = init_functions[np.random.randint(len(init_functions))]   # List or random init functions for generator
-    i_d_function = init_functions[np.random.randint(len(init_functions))]   # List or random init functions for discriminator
+    gen_act_fs = np.random.choice(act_functions, gen_n_hidden)
+    gen_act_fs = (np.append(gen_act_fs, [None])).tolist()
+    disc_act_fs = np.random.choice(act_functions, disc_n_hidden)
+    disc_act_fs = (np.append(disc_act_fs, [None])).tolist()
 
-    generator_init_functions = []
-    discriminator_init_functions = []
+    latent_distribution_function = lat_functions[np.random.randint(len(lat_functions))]
 
-    generator_act_functions = []
-    discriminator_act_functions = []
+    lrate = np.random.choice(l_rates)
 
-    for i in range(generator_n_hidden+1):
-        generator_init_functions.append(i_g_function)
-        if i == generator_n_hidden:
-            generator_act_functions.append(None)        # Activation functions for all layers in generator
-        else:
-            generator_act_functions.append(act_functions[np.random.randint(len(act_functions))])
+    my_gan_descriptor = GANDescriptor(n, z_dim, latent_distribution_function, f_measure, lrate)
 
-    for i in range(discriminator_n_hidden+1):
-        discriminator_init_functions.append(i_d_function)
-        if i == discriminator_n_hidden:
-            discriminator_act_functions.append(None)        # Activation functions for all layers in discriminator
-        else:
-            discriminator_act_functions.append(act_functions[np.random.randint(len(act_functions))])
-
-    latent_distribution_function = lat_functions[np.random.randint(len(lat_functions))]             # Distribution for latent random samples
-
-    lrate = np.random.choice(lrates)
-
-    my_gan_descriptor = GANDescriptor(X_dim, z_dim, latent_distribution_function, fmeasure, lrate)
-
-    my_gan_descriptor.gan_discriminator_initialization(generator_n_hidden, generator_dim_list,
-                                                       generator_init_functions,  generator_act_functions, gen_number_loop_train)
-    my_gan_descriptor.gan_generator_initialization(discriminator_n_hidden, discriminator_dim_list,
-                                                   discriminator_init_functions,  discriminator_act_functions, disc_number_loop_train)
+    my_gan_descriptor.gan_generator_initialization(gen_n_hidden, gen_dims, gen_init_fs,  gen_act_fs, gen_loop)
+    my_gan_descriptor.gan_discriminator_initialization(disc_n_hidden, disc_dims, disc_init_fs,  disc_act_fs, disc_loop)
 
     ind = ind_class(my_gan_descriptor)
 
@@ -182,14 +159,14 @@ def mut_gan(individual):
     else:                                       # Generator network will be mutated
         aux_network = my_gan_descriptor.Gen_network
 
-    if aux_network.number_hidden_layers < nlayers:
+    if aux_network.number_hidden_layers < n_layers:
         type_mutation = mutation_types[np.random.randint(len(mutation_types))]
 
     else:
         type_mutation = mutation_types[np.random.randint(1, len(mutation_types))]
 
     if type_mutation == "network_loops":       # The number of loops for the network learning is changed
-        aux_network.number_loop_train = np.random.randint(nloops)+1
+        aux_network.number_loop_train = np.random.randint(n_loops)+1
 
     elif type_mutation == "add_layer":             # We add one layer
         layer_pos = np.random.randint(aux_network.number_hidden_layers)+1
@@ -214,7 +191,7 @@ def mut_gan(individual):
         aux_network.change_dimensions_in_random_layer(max_layer_size)
 
     elif type_mutation == "divergence":             # We change the divergence measure used by the GAN
-        fmeasure = divergence_measures[np.random.randint(len(divergence_measures))]
+        fmeasure = div_measures[np.random.randint(len(div_measures))]
         my_gan_descriptor.fmeasure = fmeasure
 
     elif type_mutation == "latent":             # We change the divergence measure used by the GAN
@@ -222,7 +199,7 @@ def mut_gan(individual):
         my_gan_descriptor.latent_distribution_function = latent_distribution
 
     elif type_mutation == "lrate":
-        my_gan_descriptor.lrate = np.random.choice(lrates)
+        my_gan_descriptor.lrate = np.random.choice(l_rates)
 
     return individual,
 
@@ -253,7 +230,7 @@ def init_ga():
     if SEL == 0:
         toolbox.register("select", tools.selBest)
     elif SEL == 1:
-        toolbox.register("select", tools.selTournament, tournsize=tournsel_size)
+        toolbox.register("select", tools.selTournament, tournsize=tourn_sel_size)
     elif SEL == 2:
         toolbox.register("select", tools.selNSGA2)
 
@@ -293,47 +270,35 @@ if __name__ == "__main__":  # Example python3 GAN_Descriptor_Deap.py 0 1000 10 1
     parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum,
                         default=max, help='sum the integers (default: find the max)')
 
-    # The parameters of the program are set or read from command line
-
-    global All_Evals
-    global best_val
-    global Eval_Records
-    global test1
-    global test2
-
     All_Evals = 0
     best_val = 10000
     Eval_Records = None
 
     args = parser.parse_args()
-    myseed = args.integers[0]               # Seed: Used to set different outcomes of the stochastic program
+    my_seed = args.integers[0]               # Seed: Used to set different outcomes of the stochastic program
     number_samples = args.integers[1]       # Number samples to generate by the GAN (number_samples=1000)
     n = args.integers[2]                    # Number of Variables of the MO function (n=10)
-    Function = "F"+str(args.integers[3])    # MO function to optimize, all in F1...F9, except F6
+    Function = "F" + str(args.integers[3])    # MO function to optimize, all in F1...F9, except F6
     z_dim = args.integers[4]                # Dimension of the latent variable  (z_dim=30)
-    nlayers = args.integers[5]              # Maximum number of layers for generator and discriminator (nlayers = 10)
-    nloops = args.integers[6]               # Maximum number of loops for training a network  (nloops = 5)
+    n_layers = args.integers[5]              # Maximum number of layers for generator and discriminator (nlayers = 10)
+    n_loops = args.integers[6]               # Maximum number of loops for training a network  (nloops = 5)
     max_layer_size = args.integers[7]       # Maximum size of the layers  (max_layer_size = 50)
-    npop = args.integers[8]                 # Population size
-    ngen = args.integers[9]                 # Number of generations
+    n_pop = args.integers[8]                 # Population size
+    n_gen = args.integers[9]                 # Number of generations
     SEL = args.integers[10]                 # Selection method
     CXp = args.integers[11]*0.01            # Crossover probability (Mutation is 1-CXp)
-    nselpop = args.integers[12]             # Selected population size
+    n_sel_pop = args.integers[12]             # Selected population size
     if len(args.integers) > 13:
-        tournsel_size = args.integers[13]   # Tournament value
+        tourn_sel_size = args.integers[13]   # Tournament value
     else:
-        tournsel_size = 4
+        tourn_sel_size = 4
 
-    All_Evals = 0                                     # Tracks the number of evaluations
-    best_val = 10000.0                                # Tracks best value among solutions
-
-    k = 1000                                          # Number of samples of the Pareto set for computing approximation (k=1000)
+    k = 1000                                  # Number of samples of the Pareto set for computing approximation (k=1000)
     MOP_f = FFunctions(n, Function)                   # Creates a class containing details on MOP
     ps_all_x = MOP_f.generate_ps_samples(k)           # Generates k points from the Pareto Set
     test = MOP_f.generate_ps_samples(k)
     pf1, pf2 = MOP_f.evaluate_mop_function(ps_all_x)  # Evaluate the points from the Pareto Set
-    test1, test2 = MOP_f.evaluate_mop_function(ps_all_x)
-    X_dim = n                                         # Number of variables to approximate
+    test1, test2 = MOP_f.evaluate_mop_function(test)
 
     # List of activation functions the networks can use
     act_functions = [None, tf.nn.relu, tf.nn.elu, tf.nn.softplus,
@@ -346,23 +311,25 @@ if __name__ == "__main__":  # Example python3 GAN_Descriptor_Deap.py 0 1000 10 1
     lat_functions = [np.random.uniform, np.random.normal]
 
     #  List of divergence measures
-    divergence_measures = ["Total_Variation", "Standard_Divergence", "Forward_KL", "Reverse_KL", "Pearson_Chi_squared", "Least_squared"]
-    lrates = [0.00001]#[0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005]
+    div_measures = list(Divergence_Fs.keys())
+
+    l_rates = [0.0001]
     # Mutation types
-    mutation_types = ["add_layer", "network_loops", "del_layer", "weigt_init", "activation", "dimension", "divergence", "latent", "lrate"]
+    mutation_types = ["add_layer", "network_loops", "del_layer", "weigt_init", "activation", "dimension", "divergence",
+                      "latent", "lrate"]
 
     mb_size = 150           # Minibatch size
     number_epochs = 1001    # Number epochs for training
-    print_cycle = 1001      # Frequency information is printed
+    print_cycle = 1002      # Frequency information is printed
 
     # GA initialization
     toolb = init_ga()
 
     # Runs the GA
-    res, logbook, hof = aplly_ga_gan(toolb, pop_size=npop, gen_number=ngen, cxpb=CXp, mutpb=1-CXp)
+    res, logbook, hof = aplly_ga_gan(toolb, pop_size=n_pop, gen_number=n_gen, cxpb=CXp, mutpb=1-CXp)
 
     # Save the configurations of all networks evaluated
-    fname = "GAN_Evals_"+str(myseed)+"_"+Function+"_N_"+str(npop)+"_ngen_"+str(ngen)+"_Sel_"+str(SEL)+"_X_" + str(X_dim)
+    fname = "ResultsPareto"+str(my_seed)+"_"+Function+"_"+str(n_pop)+"_"+str(n_gen)+"_"+str(SEL)+"_" + str(n)
     np.save(fname, Eval_Records)
 
     # Examples of how to call the function
